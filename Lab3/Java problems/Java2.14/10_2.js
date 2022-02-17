5a02a1544e0aa193338f3915d77e4a0a1fc68ca1
yerkebulanauyelbek <78300056+yerkebulanauyelbek@users.noreply.github.com>
2022-02-17 18:53:39 +0600

    Add files via upload

---
+++ Lab3/Java problems/Java2.14/10_2.js
@@ -0,0 +1,13 @@
+const number=+prompt('Type number between 0 and 3','');
+switch(number){
+    case 0:
+        alert('You typed number 0');
+        break;
+    case 1:
+        alert('You typed number 1');
+        break;
+    case 2:
+    case 3:
+        alert('You typed number 2, or maybe 3')
+        break;
+}
