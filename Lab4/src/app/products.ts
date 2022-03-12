72758015090b764e7742fcc196222bacb940c825
yerkebulanauyelbek <78300056+yerkebulanauyelbek@users.noreply.github.com>
2022-03-13 02:11:02 +0600

    Add files via upload

---
+++ Lab4/src/app/products.ts
@@ -0,0 +1,87 @@
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+  rating: string;
+  image: string;
+  link: string;
+}
+
+export const products = [
+  {
+    id: 1,
+    name: 'Apple iPhone 11 Pro, 256GB, Space Gray',
+    price: 446,
+    description:
+      'Model Name IPhone 11 Pro Wireless Carrier Unlocked Brand Apple Form Factor Smartphone Memory Storage Capacity 256 GB Operating System IOS Color Space Gray Cellular Technology 4G SIM card slot count Single SIM Year 2018',
+    rating: '4.4 / 5',
+    image: 'https://m.media-amazon.com/images/I/81x9I9qXbmL._AC_SY550_.jpg',
+    link: 'https://www.amazon.com/Apple-iPhone-256GB-Space-Gray/dp/B08BHPMTG5/ref=sr_1_1?keywords=Apple%2BiPhone%2B11%2BPro&qid=1647097925&sr=8-1&th=1',
+  },
+  {
+    id: 2,
+    name: 'Apple iPhone 12 Pro, 256GB, Pacific Blue',
+    price: 849,
+    description:
+      'Model Name IPhone 12 Pro Wireless Carrier Unlocked Brand Apple Form Factor Smartphone Memory Storage Capacity 256 GB Operating System IOS 12 Color Pacific Blue Cellular Technology LTE Included Components SIM Tray Ejector, USB Cable Display Type OLED',
+    rating: '3.5 / 5',
+    image: 'https://m.media-amazon.com/images/I/712yl2wTDbL._AC_SX522_.jpg',
+    link: 'https://www.amazon.com/Apple-iPhone-12-Pro-Pacific/dp/B09JFN8K6T',
+  },
+  {
+    id: 3,
+    name: 'Apple iPhone 12, 64GB, Blue',
+    price: 659,
+    description:
+      'Model Name IPhone 12 Wireless Carrier Unlocked Brand Apple Form Factor Smartphone Memory Storage Capacity 64 GB Operating System IOS 14 Color Blue Cellular Technology 5G Included Components Charging cable and block, and sim tray removal tool Screen Size 6.1 Inches',
+    rating: '5 / 5',
+    image: 'https://m.media-amazon.com/images/I/7117DN7UkKL._AC_SY550_.jpg',
+    link: 'https://www.amazon.com/Apple-iPhone-12-64GB-Blue/dp/B09JFTPQY1/ref=sr_1_5?crid=11JR58TEYJK1J&keywords=iphone&qid=1646856265&sprefix=iphone%2Caps%2C337&sr=8-5',
+  },
+  {
+    id: 4,
+    name: 'Samsung Galaxy S21 FE 5G Cell Phone, Factory Unlocked Android Smartphone, 128GB, 120Hz Display, Pro Grade Camera, All Day Intelligent Battery, US Version, White',
+    price: 435,
+    description:
+      'Model Name S21 FE Wireless Carrier Unlocked Brand Samsung Electronics Form Factor Smartphone Memory Storage Capacity 6 GB Operating System Android Color White Cellular Technology 5G Year 2021',
+    rating: '4.6 / 5',
+    image: 'https://m.media-amazon.com/images/I/61kLsk8RslL._AC_SL1500_.jpg',
+    link: 'https://www.amazon.com/Samsung-Factory-Unlocked-Smartphone-Intelligent/dp/B09BFRV59N/ref=sr_1_1?crid=3Q2MWCF0M7NDF&keywords=samsung&qid=1647112195&sprefix=samsung%2Caps%2C339&sr=8-1&th=1',
+  },
+  {
+    id: 5,
+    name: 'Samsung Galaxy S22 Ultra Smartphone, Factory Unlocked Android Cell Phone, 128GB, 8K Camera & Video, Brightest Display, S Pen, Long Battery Life, Fast 4nm Processor, US Version, Phantom Black',
+    price: 359,
+    description:
+      'Model Name S22 Ultra Wireless Carrier Unlocked for All Carriers Brand Samsung Electronics Form Factor Smartphone Memory Storage Capacity 8 GB Operating System Android Color Phantom Black Cellular Technology 5G, 4G LTE Year 2022 Included Components USB',
+    rating: '3.5 / 5',
+    image: 'https://m.media-amazon.com/images/I/61U6oC65TTL._AC_SL1500_.jpg',
+    link: 'https://www.amazon.com/Samsung-Smartphone-Unlocked-Brightest-Processor/dp/B09MVZSW5V/ref=sr_1_2?crid=2HOIHE7IIVEEL&keywords=samsung&qid=1647113060&sprefix=samsung%2Caps%2C479&sr=8-2&th=1',
+  },
+  {
+    id: 6,
+    name: 'Apple AirPods (3rd Generation)',
+    price: 135,
+    description:
+      'Brand Apple Ear Placement In Ear Color White Connectivity Technology Wireless, Apple H1 Chip Model Name AirPods',
+    rating: '4.1 / 5',
+    image: 'https://m.media-amazon.com/images/I/61ZRU9gnbxL._AC_SL1500_.jpg',
+    link: 'https://www.amazon.com/Apple-AirPods-3rd-Generation-Renewed/dp/B09M94KXPN/ref=sr_1_5?keywords=airpods&qid=1646859097&sprefix=air%2Caps%2C331&sr=8-5',
+  },
+  {
+    id: 7,
+    name: 'Samsung Galaxy Buds Plus, True Wireless Earbuds Bluetooth 5.0 (Wireless Charging Case Included), Black – US Version',
+    price: 115,
+    description:
+      'Brand Samsung Electronics Ear Placement In Ear Color Black Connectivity Technology Wireless Model Name Galaxy Buds+',
+    rating: '4.1 / 5',
+    image: 'https://m.media-amazon.com/images/I/614bL2rQtcL._AC_SL1468_.jpg',
+    link: 'https://www.amazon.com/Samsung-Wireless-improved-Charging-included/dp/B083KVM9VW/ref=sr_1_6?crid=2MPVIQRIZHAHX&keywords=headphones%2Bsamsung&qid=1647113419&sprefix=headphones%2Bsamsun%2Caps%2C251&sr=8-6&th=1',
+  },
+];
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
